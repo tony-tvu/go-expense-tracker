686fc83d18afc5ea0c94ec35d33e0d5893664d1a
tony-tvu <Tony.T.Vu@outlook.com>
2022-08-30 08:38:22 -0500

    support multiple allowed origins from env

--- middleware/cors.go
+++ middleware/cors.go
@@ -2,25 +2,27 @@ package middleware
 
 import (
 	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
-var subdomain string
+var allowedOrigins []string
 
 func init() {
 	godotenv.Load(".env")
-	subdomain = os.Getenv("SUB_DOMAIN")
+	allowedOrigins = strings.Split(os.Getenv("ALLOWED_ORIGIN_DOMAINS"), ",")
 }
 
 func CORS(env *string) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		if *env == "development" {
 			c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		}
-		if subdomain != "" {
-			c.Writer.Header().Set("Access-Control-Allow-Origin", subdomain)
+		} else if len(allowedOrigins) != 0 {
+			for _, origin := range allowedOrigins {
+				c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+			}
 		}
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Plaid-Public-Token")
