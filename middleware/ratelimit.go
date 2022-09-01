d66418965e00698fc41c4dfb55d0d3c1c8a1eeba
tony-tvu <Tony.T.Vu@outlook.com>
2022-09-01 07:51:50 -0500

    add rate limit configs to .env

--- middleware/ratelimit.go
+++ middleware/ratelimit.go
@@ -1,15 +1,27 @@
 package middleware
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/ulule/limiter/v3"
 	mgin "github.com/ulule/limiter/v3/drivers/middleware/gin"
 	"github.com/ulule/limiter/v3/drivers/store/memory"
 )
 
+var globalRate string
+var loginRate string
+
+func init() {
+	godotenv.Load(".env")
+	globalRate = os.Getenv("GLOBAL_RATE_LIMIT")
+	loginRate = os.Getenv("LOGIN_RATE_LIMIT")
+}
+
 // Middleware applies to all routes
 func RateLimit() gin.HandlerFunc {
-	rate, err := limiter.NewRateFromFormatted("300-M")
+	rate, err := limiter.NewRateFromFormatted(globalRate)
 	if err != nil {
 		panic(err)
 	}
@@ -20,7 +32,7 @@ func RateLimit() gin.HandlerFunc {
 
 // Middleware applies to login route
 func LoginRateLimit() gin.HandlerFunc {
-	rate, err := limiter.NewRateFromFormatted("5-M")
+	rate, err := limiter.NewRateFromFormatted(loginRate)
 	if err != nil {
 		panic(err)
 	}
