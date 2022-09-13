bf5dfb5a802cefbe1f9bf2ca6f689de52c829c44
tony-tvu <Tony.T.Vu@outlook.com>
2022-09-13 08:29:46 -0500

    make separate tests

--- tests/auth_test.go
+++ tests/auth_test.go
@@ -7,154 +7,156 @@ import (
 	"github.com/tony-tvu/goexpense/entity"
 )
 
-func TestAuth(t *testing.T) {
-	t.Run("IsAuthorized should issue access tokens correctly", func(t *testing.T) {
-		t.Parallel()
+// VerifyUsers should issue access tokens correctly
+func TestAuthTokens(t *testing.T) {
+	t.Parallel()
 
-		// create user and login
-		username := "isauthorized"
-		email := "isauthorized@email.com"
-		password := "password"
-		_, cleanup := createUser(t, username, email, password)
-		defer cleanup()
-		accessToken, refreshToken, _ := logUserIn(t, username, password)
+	// create user and login
+	username := "isauthorized"
+	email := "isauthorized@email.com"
+	password := "password"
+	_, cleanup := createUser(t, username, email, password)
+	defer cleanup()
+	accessToken, refreshToken, _ := logUserIn(t, username, password)
 
-		// make request to authRequired endpoint
-		query :=
-			`query {
+	// make request to authRequired endpoint
+	query :=
+		`query {
+			userInfo {
+				id
+			}
+		}`
+	res, qlRes := doQL(t, &accessToken, &refreshToken, query)
+	assert.Nil(t, qlRes.Errors)
+
+	// should not return a new access token
+	cookies := getCookies(t, res.Cookies())
+	assert.Equal(t, "", cookies["goexpense_access"])
+
+	// make request with mocked expired access token
+	res, qlRes = doQL(t, nil, &refreshToken, query)
+
+	// should return new access token and have no errors
+	assert.Nil(t, qlRes.Errors)
+	cookies = getCookies(t, res.Cookies())
+	assert.NotEqual(t, accessToken, cookies["goexpense_access"])
+}
+
+// Resolvers should return error for revoked sessions
+func TestAuthRevokeTokens(t *testing.T) {
+	t.Parallel()
+
+	// create user and login
+	username := "revokeMe"
+	email := "revokeMe@email.com"
+	password := "^%#(GY%H=G$%asdf"
+	user, cleanup := createUser(t, username, email, password)
+	defer cleanup()
+	accessToken, refreshToken, _ := logUserIn(t, username, password)
+
+	// make request to authRequired endpoint
+	query :=
+		`query {
+			userInfo {
+				id
+			}
+		}`
+	_, qlRes := doQL(t, &accessToken, &refreshToken, query)
+	assert.Nil(t, qlRes.Errors)
+
+	// make request to authRequired endpoint with expired accessToken
+	_, qlRes = doQL(t, nil, &refreshToken, query)
+	assert.Nil(t, qlRes.Errors)
+
+	// revoke token
+	if result := testApp.Db.Exec("DELETE FROM sessions WHERE user_id = ?", user.ID); result.Error != nil {
+		t.FailNow()
+	}
+
+	// make request to authRequired endpoint with expired accessToken
+	_, qlRes = doQL(t, nil, &refreshToken, query)
+
+	// should return error
+	assert.Equal(t, "not authorized", qlRes.Errors[0].Message)
+}
+
+// Auth restricted resolvers should not allow access to guest users
+func TestAuthRestrictAccess(t *testing.T) {
+	t.Parallel()
+
+	// make request to endpoint where user must be logged in without access token
+	query :=
+		`query {
 				userInfo {
 					id
 				}
 			}`
-		res, qlRes := doQL(t, &accessToken, &refreshToken, query)
-		assert.Nil(t, qlRes.Errors)
+	_, qlRes := doQL(t, nil, nil, query)
 
-		// should not return a new access token
-		cookies := getCookies(t, res.Cookies())
-		assert.Equal(t, "", cookies["goexpense_access"])
+	// should return auth error
+	assert.Equal(t, "not authorized", qlRes.Errors[0].Message)
 
-		// make request with mocked expired access token
-		res, qlRes = doQL(t, nil, &refreshToken, query)
+	// make request to same endpoint with invalid access token
+	invalidToken := "invalidToken"
+	res, qlRes := doQL(t, &invalidToken, nil, query)
 
-		// should return new access token and have no errors
-		assert.Nil(t, qlRes.Errors)
-		cookies = getCookies(t, res.Cookies())
-		assert.NotEqual(t, accessToken, cookies["goexpense_access"])
-	})
+	// should return auth error
+	assert.Equal(t, "not authorized", qlRes.Errors[0].Message)
 
-	t.Run("IsAuthorized should return error for revoked sessions", func(t *testing.T) {
-		t.Parallel()
+	// response should not have new token cookie
+	cookies := getCookies(t, res.Cookies())
+	assert.Equal(t, "", cookies["goexpense_access"])
+	assert.Equal(t, "", cookies["goexpense_refresh"])
+}
 
-		// create user and login
-		username := "revokeMe"
-		email := "revokeMe@email.com"
-		password := "^%#(GY%H=G$%asdf"
-		user, cleanup := createUser(t, username, email, password)
-		defer cleanup()
-		accessToken, refreshToken, _ := logUserIn(t, username, password)
+// Admin restricted resolvers should not allow guest or regular users
+func TestAuthAdminRestricted(t *testing.T) {
+	t.Parallel()
 
-		// make request to authRequired endpoint
-		query :=
-			`query {
-				userInfo {
-					id
-				}
-			}`
-		_, qlRes := doQL(t, &accessToken, &refreshToken, query)
-		assert.Nil(t, qlRes.Errors)
+	// make request to admin-only route as a guest user
+	query :=
+		`query {
+			sessions {
+				id
+			}
+		}`
+	_, qlRes := doQL(t, nil, nil, query)
 
-		// make request to authRequired endpoint with expired accessToken
-		_, qlRes = doQL(t, nil, &refreshToken, query)
-		assert.Nil(t, qlRes.Errors)
+	// should return auth error
+	assert.Equal(t, "not authorized", qlRes.Errors[0].Message)
 
-		// revoke token
-		if result := testApp.Db.Exec("DELETE FROM sessions WHERE user_id = ?", user.ID); result.Error != nil {
-			t.FailNow()
-		}
+	// create regular user and login
+	username := "IsAdmin"
+	email := "IsAdmin@email.com"
+	password := "^%#(GY%H=G$%asdf"
+	user, cleanup := createUser(t, username, email, password)
+	defer cleanup()
+	accessToken, refreshToken, _ := logUserIn(t, username, password)
 
-		// make request to authRequired endpoint with expired accessToken
-		_, qlRes = doQL(t, nil, &refreshToken, query)
+	// make request to admin-only route as regular user
+	_, qlRes = doQL(t, &accessToken, &refreshToken, query)
 
-		// should return error
-		assert.Equal(t, "not authorized", qlRes.Errors[0].Message)
-	})
+	// should return auth error
+	assert.Equal(t, "not authorized", qlRes.Errors[0].Message)
 
-	t.Run("IsAuthorized should not allow access to guest users", func(t *testing.T) {
-		t.Parallel()
+	// logout user
+	logoutQ :=
+		`mutation {
+			logout
+		}`
+	_, qlRes = doQL(t, &accessToken, &refreshToken, logoutQ)
+	assert.Nil(t, qlRes.Errors)
 
-		// make request to endpoint where user must be logged in without access token
-		query :=
-			`query {
-				userInfo {
-					id
-				}
-			}`
-		_, qlRes := doQL(t, nil, nil, query)
+	// make user an admin and login
+	if result := testApp.Db.Model(&entity.User{}).Where("id = ?", user.ID).Update("type", entity.AdminUser); result.Error != nil {
+		t.FailNow()
+	}
 
-		// should return auth error
-		assert.Equal(t, "not authorized", qlRes.Errors[0].Message)
+	accessToken, refreshToken, _ = logUserIn(t, username, password)
 
-		// make request to same endpoint with invalid access token
-		invalidToken := "invalidToken"
-		res, qlRes := doQL(t, &invalidToken, nil, query)
+	// make request to admin-only endpoint as admin
+	_, qlRes = doQL(t, &accessToken, &refreshToken, query)
 
-		// should return auth error
-		assert.Equal(t, "not authorized", qlRes.Errors[0].Message)
-
-		// response should not have new token cookie
-		cookies := getCookies(t, res.Cookies())
-		assert.Equal(t, "", cookies["goexpense_access"])
-		assert.Equal(t, "", cookies["goexpense_refresh"])
-	})
-
-	t.Run("IsAdmin should not allow access to guest or regular users", func(t *testing.T) {
-		t.Parallel()
-
-		// make request to admin-only route as a guest user
-		query :=
-			`query {
-				sessions {
-					id
-				}
-			}`
-		_, qlRes := doQL(t, nil, nil, query)
-
-		// should return auth error
-		assert.Equal(t, "not authorized", qlRes.Errors[0].Message)
-
-		// create regular user and login
-		username := "IsAdmin"
-		email := "IsAdmin@email.com"
-		password := "^%#(GY%H=G$%asdf"
-		user, cleanup := createUser(t, username, email, password)
-		defer cleanup()
-		accessToken, refreshToken, _ := logUserIn(t, username, password)
-
-		// make request to admin-only route as regular user
-		_, qlRes = doQL(t, &accessToken, &refreshToken, query)
-
-		// should return auth error
-		assert.Equal(t, "not authorized", qlRes.Errors[0].Message)
-
-		// logout user
-		logoutQ :=
-			`mutation {
-				logout
-			}`
-		_, qlRes = doQL(t, &accessToken, &refreshToken, logoutQ)
-		assert.Nil(t, qlRes.Errors)
-
-		// make user an admin and login
-		if result := testApp.Db.Model(&entity.User{}).Where("id = ?", user.ID).Update("type", entity.AdminUser); result.Error != nil {
-			t.FailNow()
-		}
-
-		accessToken, refreshToken, _ = logUserIn(t, username, password)
-
-		// make request to admin-only endpoint as admin
-		_, qlRes = doQL(t, &accessToken, &refreshToken, query)
-
-		// should not have errors
-		assert.Nil(t, qlRes.Errors)
-	})
+	// should not have errors
+	assert.Nil(t, qlRes.Errors)
 }
--- tests/user_test.go
+++ tests/user_test.go
@@ -11,81 +11,82 @@ import (
 	"gorm.io/gorm"
 )
 
-func TestUserResolvers(t *testing.T) {
-	t.Run("Login and Logout resolvers work correctly", func(t *testing.T) {
-		t.Parallel()
+// Login and Logout resolvers work correctly
+func TestLoginAndLogout(t *testing.T) {
+	t.Parallel()
 
-		// create user
-		username := "loginTestUser"
-		email := "loginTestUser@email.com"
-		password := "password"
-		user, cleanup := createUser(t, username, email, password)
-		defer cleanup()
+	// create user
+	username := "loginTestUser"
+	email := "loginTestUser@email.com"
+	password := "password"
+	user, cleanup := createUser(t, username, email, password)
+	defer cleanup()
 
-		// login with wrong password
-		_, _, qlRes := logUserIn(t, username, "wrong")
+	// login with wrong password
+	_, _, qlRes := logUserIn(t, username, "wrong")
 
-		// should return error
-		assert.Equal(t, "not authorized", qlRes.Errors[0].Message)
+	// should return error
+	assert.Equal(t, "not authorized", qlRes.Errors[0].Message)
 
-		// login with unknown username
-		_, _, qlRes = logUserIn(t, "userNameDoesntExist", password)
+	// login with unknown username
+	_, _, qlRes = logUserIn(t, "userNameDoesntExist", password)
 
-		// should return unknown user error
-		assert.Equal(t, "user not found", qlRes.Errors[0].Message)
+	// should return unknown user error
+	assert.Equal(t, "user not found", qlRes.Errors[0].Message)
 
-		// login with correct credentials
-		query := fmt.Sprintf(
-			`mutation {
-				login(
-					input: {
-						username: "%s"
-						password: "%s"
-					}
-				)
-			}`, username, password,
-		)
-		res, qlRes := doQL(t, nil, nil, query)
+	// login with correct credentials
+	query := fmt.Sprintf(
+		`mutation {
+			login(
+				input: {
+					username: "%s"
+					password: "%s"
+				}
+			)
+		}`, username, password,
+	)
+	res, qlRes := doQL(t, nil, nil, query)
 
-		// should not have errors
-		assert.Nil(t, qlRes.Errors)
+	// should not have errors
+	assert.Nil(t, qlRes.Errors)
 
-		// should have user session saved in db
-		var s entity.Session
-		result := testApp.Db.Where("user_id = ?", user.ID).First(&s)
-		assert.Nil(t, result.Error)
+	// should have user session saved in db
+	var s entity.Session
+	result := testApp.Db.Where("user_id = ?", user.ID).First(&s)
+	assert.Nil(t, result.Error)
 
-		// logout
-		cookies := getCookies(t, res.Cookies())
-		accessToken := cookies["goexpense_access"]
-		refreshToken := cookies["goexpense_refresh"]
-		logoutQ :=
-			`mutation {
-			logout
-		}`
-		_, qlRes = doQL(t, &accessToken, &refreshToken, logoutQ)
-		assert.Nil(t, qlRes.Errors)
+	// logout
+	cookies := getCookies(t, res.Cookies())
+	accessToken := cookies["goexpense_access"]
+	refreshToken := cookies["goexpense_refresh"]
+	logoutQ :=
+		`mutation {
+		logout
+	}`
+	_, qlRes = doQL(t, &accessToken, &refreshToken, logoutQ)
+	assert.Nil(t, qlRes.Errors)
 
-		// should no longer have user session saved after logging out
-		result = testApp.Db.Where("username = ?", username).First(&s)
-		assert.Equal(t, gorm.ErrRecordNotFound, result.Error)
-	})
+	// should no longer have user session saved after logging out
+	result = testApp.Db.Where("username = ?", username).First(&s)
+	assert.Equal(t, gorm.ErrRecordNotFound, result.Error)
+}
 
-	t.Run("UserInfo resolver returns correct information", func(t *testing.T) {
-		t.Parallel()
+// UserInfo resolver returns correct information
+func TestUserInfo(t *testing.T) {
+	t.Parallel()
 
-		// create user
-		username := "UserInfo"
-		email := "UserInfo@email.com"
-		password := "^%#(GY%H=G$%asdf"
-		_, cleanup := createUser(t, username, email, password)
-		defer cleanup()
+	// create user
+	username := "UserInfo"
+	email := "UserInfo@email.com"
+	password := "^%#(GY%H=G$%asdf"
+	_, cleanup := createUser(t, username, email, password)
+	defer cleanup()
 
-		accessToken, refreshToken, _ := logUserIn(t, username, password)
+	accessToken, refreshToken, _ := logUserIn(t, username, password)
 
-		// make request
-		query :=
-			`query {
+	// make request
+	query :=
+		`query {
 				userInfo {
 					id
 					username
@@ -95,24 +96,23 @@ func TestUserResolvers(t *testing.T) {
 					updatedAt
 				}
 			}`
-		_, qlRes := doQL(t, &accessToken, &refreshToken, query)
+	_, qlRes := doQL(t, &accessToken, &refreshToken, query)
 
-		// should not have errors
-		assert.Nil(t, qlRes.Errors)
+	// should not have errors
+	assert.Nil(t, qlRes.Errors)
 
-		// should have correct user info returned
-		var data struct {
-			UserInfo entity.User
-		}
+	// should have correct user info returned
+	var data struct {
+		UserInfo entity.User
+	}
 
-		err := json.Unmarshal(qlRes.Data, &data)
-		require.NoError(t, err)
+	err := json.Unmarshal(qlRes.Data, &data)
+	require.NoError(t, err)
 
-		assert.NotNil(t, data.UserInfo.ID)
-		assert.Equal(t, username, data.UserInfo.Username)
-		assert.Equal(t, email, data.UserInfo.Email)
-		assert.Equal(t, entity.RegularUser, data.UserInfo.Type)
-		assert.NotNil(t, data.UserInfo.CreatedAt)
-		assert.NotNil(t, data.UserInfo.UpdatedAt)
-	})
+	assert.NotNil(t, data.UserInfo.ID)
+	assert.Equal(t, username, data.UserInfo.Username)
+	assert.Equal(t, email, data.UserInfo.Email)
+	assert.Equal(t, entity.RegularUser, data.UserInfo.Type)
+	assert.NotNil(t, data.UserInfo.CreatedAt)
+	assert.NotNil(t, data.UserInfo.UpdatedAt)
 }
