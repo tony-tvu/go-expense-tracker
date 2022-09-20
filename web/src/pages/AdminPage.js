40c16022526988c0362ce8e9ac61c1e696cffd50
tony-tvu <Tony.T.Vu@outlook.com>
2022-09-19 19:12:18 -0500

    [WIP] add admin configs

--- web/src/pages/AdminPage.js
+++ web/src/pages/AdminPage.js
@@ -8,12 +8,19 @@ import {
   Text,
   useColorModeValue,
   Container,
+  Divider,
+  Switch,
+  Stack,
+  Select,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
 } from '@chakra-ui/react'
 import logger from '../logger'
 
 export default function AdminPage() {
-
-
   const stackBgColor = useColorModeValue('white', 'gray.900')
 
   const getConfigs = useCallback(async () => {
@@ -27,7 +34,7 @@ export default function AdminPage() {
       .then(async (res) => {
         if (!res) return
         const data = await res.json().catch((err) => logger(err))
-        // console.log(data)
+        console.log(data)
       })
       .catch((err) => {
         logger('error getting items', err)
@@ -40,22 +47,108 @@ export default function AdminPage() {
 
   return (
     <VStack>
-      <Container maxW="container.md" centerContent>
-        <Text fontSize="2xl" as="b" mb={5} mt={5}>
-          Configuration
-        </Text>
+      <Container maxW="container.md" mt={3}>
+        <FormControl bg={stackBgColor} p={5}>
+          <FormLabel fontSize="xl">App configuration</FormLabel>
+          <Divider mb={5} />
+          <FormLabel mt={5}>Access Token Expiration</FormLabel>
+          <FormLabel fontSize={'xs'} color={'gray.500'}>
+            Default: 900 seconds
+          </FormLabel>
+          <NumberInput
+            defaultValue={15}
+            min={1}
+            max={2592000}
+            onChange={(value) => console.log(value)}
+          >
+            <NumberInputField />
+            <NumberInputStepper>
+              <NumberIncrementStepper />
+              <NumberDecrementStepper />
+            </NumberInputStepper>
+          </NumberInput>
 
-        <FormControl
-          borderWidth="1px"
-          borderRadius="lg"
-          bg={stackBgColor}
-          boxShadow={'2xl'}
-          p={5}
-          mb={5}
-        >
-          <FormLabel>Email address</FormLabel>
-          <Input type="email" />
-          <FormHelperText>We'll never share your email.</FormHelperText>
+          <FormLabel mt={5}>Refresh Token Expiration</FormLabel>
+          <FormLabel fontSize={'xs'} color={'gray.500'}>
+            Default: 3600 seconds
+          </FormLabel>
+          <NumberInput
+            defaultValue={15}
+            min={1}
+            max={2592000}
+            onChange={(value) => console.log(value)}
+          >
+            <NumberInputField />
+            <NumberInputStepper>
+              <NumberIncrementStepper />
+              <NumberDecrementStepper />
+            </NumberInputStepper>
+          </NumberInput>
+
+
+          <FormLabel mt={5}>Quota</FormLabel>
+          <Select
+            defaultValue={false}
+            onChange={(event) => console.log(event.target.value)}
+          >
+            <option value={true}>Enabled</option>
+            <option value={false}>Disabled</option>
+          </Select>
+
+
+          <FormLabel mt={5}>Quota Limit</FormLabel>
+          <NumberInput
+            defaultValue={15}
+            min={1}
+            max={2592000}
+            onChange={(value) => console.log(value)}
+          >
+            <NumberInputField />
+            <NumberInputStepper>
+              <NumberIncrementStepper />
+              <NumberDecrementStepper />
+            </NumberInputStepper>
+          </NumberInput>
+
+
+
+
+
+          <FormLabel mt={5}>Scheduled Tasks</FormLabel>
+          <Select
+            defaultValue={false}
+            onChange={(event) => console.log(event.target.value)}
+          >
+            <option value={true}>Enabled</option>
+            <option value={false}>Disabled</option>
+          </Select>
+
+          <FormLabel mt={5}>Scheduled Tasks Interval</FormLabel>
+          <FormLabel fontSize={'xs'} color={'gray.500'}>
+            Default: 60 seconds
+          </FormLabel>
+          <NumberInput
+            isDisabled={true}
+            defaultValue={15}
+            min={1}
+            max={2592000}
+            onChange={(value) => console.log(value)}
+          >
+            <NumberInputField />
+            <NumberInputStepper>
+              <NumberIncrementStepper />
+              <NumberDecrementStepper />
+            </NumberInputStepper>
+          </NumberInput>
+
+          <FormLabel mt={5}>User Registration</FormLabel>
+          <Select
+            defaultValue={false}
+            onChange={(event) => console.log(event.target.value)}
+          >
+            <option value={true}>Enabled</option>
+            <option value={false}>Disabled</option>
+          </Select>
         </FormControl>
       </Container>
     </VStack>
