44dd62d720225099081cb75c3e74d609fac0e340
tony-tvu <tony.t.vu@outlook.com>
2022-09-25 09:12:04 -0500

    fix cors middleware

--- middleware/cors.go
+++ middleware/cors.go
@@ -7,8 +7,9 @@ import (
 
 func CorsHeaders(allowedOrigins []string) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		if util.Contains(&allowedOrigins, c.Request.Header.Get("Origin")) {
-			c.Writer.Header().Set("Access-Control-Allow-Origin", "webhooksURL")
+		origin := c.Request.Header.Get("Origin")
+		if util.Contains(&allowedOrigins, origin) {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
 			c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 			c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		}
