adbddd70be003774b10fe8195b8b99edf99ec38e
tony-tvu <tony.t.vu@outlook.com>
2022-10-21 15:14:40 -0500

    filter for required envs only

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,18 +1,28 @@
 require('dotenv').config()
+const appENVs = [
+  'REACT_APP_ENV',
+  'REACT_APP_API_URL',
+  'REACT_APP_TELLER_ENV',
+  'REACT_APP_TELLER_APPLICATION_ID',
+]
 
 const define = {}
 for (const k in process.env) {
-  define[`process.env.${k}`] = JSON.stringify(process.env[k])
+  if (appENVs.includes(k)) {
+    define[`process.env.${k}`] = JSON.stringify(process.env[k])
+  }
 }
 
-require('esbuild').build({
-  entryPoints: ['./web/src/index.js'],
-  outfile: './web/build/static/app.js',
-  minify: true,
-  bundle: true,
-  loader: {
-    '.js': 'jsx',
-  },
-  plugins: [],
-  define,
-}).catch(() => process.exit(1))
+require('esbuild')
+  .build({
+    entryPoints: ['./web/src/index.js'],
+    outfile: './web/build/static/app.js',
+    minify: true,
+    bundle: true,
+    loader: {
+      '.js': 'jsx',
+    },
+    plugins: [],
+    define,
+  })
+  .catch(() => process.exit(1))
