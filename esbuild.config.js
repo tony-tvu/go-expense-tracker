440c68488bab7f921389d89207293bfa9315ee49
tony-tvu <tony.t.vu@outlook.com>
2022-10-21 14:10:20 -0500

    fix esbuild config

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,21 +1,11 @@
-import { build } from 'esbuild'
-import * as dotenv from 'dotenv'
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
+require('dotenv').config()
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
-dotenv.config({
-  path: __dirname + '/.env',
-})
 const define = {}
-
 for (const k in process.env) {
   define[`process.env.${k}`] = JSON.stringify(process.env[k])
 }
 
-build({
+require('esbuild').build({
   entryPoints: ['./web/src/index.js'],
   outfile: './web/build/static/app.js',
   minify: true,
